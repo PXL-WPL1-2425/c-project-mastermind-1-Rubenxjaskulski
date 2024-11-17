43f6179be67c5a7523caecf9364158ed1c95e7ef
RubenxJaskulski <Ruben.jaskulski@student.pxl.be>
2024-11-17 22:16:19 +0100

    Code controleren

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -17,26 +17,48 @@ namespace MijnMastermind
     public partial class MainWindow : Window
     {
         private string[] beschikbareKleuren = { "Rood", "Geel", "Oranje", "Wit", "Groen", "Blauw" };
-        private string[] geheimeCode = new string[4];
-        private Random willekeurig = new Random();
+        private string color1, color2, color3, color4;
 
         public MainWindow()
         {
-            
-            GenereerGeheimeCode();
-            
+            InitializeComponent();
+            GenereerGeheimeCode(out color1, out color2, out color3, out color4);
+            Title = "MijnMastermind" + color1 + color2 + color3 + color4;
         }
 
        
 
-        private void GenereerGeheimeCode()
-        {
-            for (int i = 0; i < 4; i++)
+        private void GenereerGeheimeCode(out string color1, out string color2, out string color3, out string color4)
+        { Random random = new Random();
+            List <string> colorList = new List <string>();
+            string randomColor;
+            for (int i = 0; i < 4; i++) 
             {
-                geheimeCode[i] = beschikbareKleuren[willekeurig.Next(beschikbareKleuren.Length)];
+                randomColor = random.Next(1,7).ToString();
+                switch (randomColor)
+                {
+                    case "1": randomColor = "Rood";
+                        break;
+                    case "2": randomColor = "Blauw";
+                        break;
+                    case "3": randomColor = "Geel";
+                        break;
+                    case "4": randomColor = "Groen";
+                        break;
+                    case "5": randomColor = "Oranje";
+                        break;
+                    case "6": randomColor = "Wit";
+                        break;
+                }
+                colorList.Add(randomColor);
             }
+            color1 = colorList[0];
+            color2 = colorList[1];
+            color3 = colorList[2];
+            color4 = colorList[3];
+            
 
-            this.Title = "Geheime Code: " + string.Join(", ", geheimeCode);
+           
         }
 
         private void comboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -117,6 +139,53 @@ namespace MijnMastermind
                     break;
             }
 
+        }
+
+        private void codeKnop_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> gColors = new List<string>
+            {
+                color1, color2, color3,color4
+            };
+            string chosenColor1 = comboBox1.Text;
+            string chosenColor2 = comboBox2.Text;
+            string chosenColor3 = comboBox3.Text;
+            string chosenColor4 = comboBox4.Text;
+
+            List<string> vColors = new List<string>
+            {
+                chosenColor1 , chosenColor2 , chosenColor3 , chosenColor4
+            };
+            for (int i = 0; i < 4; i++)
+            {
+                Label temporaryLabel = null;
+                switch (i)
+                {
+                    case 0:
+                        temporaryLabel = label1;
+                        break;
+                    case 1:
+                        temporaryLabel = label2;
+                        break;
+                    case 2:
+                        temporaryLabel = label3;
+                        break;
+
+                    case 3:
+                        temporaryLabel = label4;
+                        break;
+                }
+                if (gColors[i] == vColors[i])
+                {
+                    temporaryLabel.BorderBrush = Brushes.DarkRed;
+                    temporaryLabel.BorderThickness = new Thickness(5);
+                }
+                else if (gColors.Contains(vColors[i]))
+                {
+                    temporaryLabel.BorderBrush = Brushes.Wheat;
+                    temporaryLabel.BorderThickness = new Thickness(5);
+                }
+            }
         }
     }
 }
